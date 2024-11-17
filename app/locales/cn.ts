a720cb8634f948183f828495d798c9f9ab5f486a
xlingoo <mahaonan1@hotmail.com>
2024-11-18 02:05:53 +0800

    Update cn.ts

--- app/locales/cn.ts
+++ app/locales/cn.ts
@@ -596,7 +596,7 @@ const cn = {
   },
   Store: {
     DefaultTopic: "新的聊天",
-    BotHello: "HorseGPT 很乐意帮助你",
+    BotHello: "想和HorseGPT聊什么？😊",
     Error: "出错了，稍后重试吧",
     Prompt: {
       History: (content: string) => "这是历史聊天总结作为前情提要：" + content,
