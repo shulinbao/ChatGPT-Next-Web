2990fd0f29add92ac573ee7dae06be13b60d7681
xlingoo <mahaonan1@hotmail.com>
2024-11-18 01:05:06 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,10 +10,10 @@ import { GoogleTagManager, GoogleAnalytics } from "@next/third-parties/google";
 const serverConfig = getServerSideConfig();
 
 export const metadata: Metadata = {
-  title: "NextChat",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "HorseGPT",
+  description: "Chat website powered by the HorseGPT project.",
   appleWebApp: {
-    title: "NextChat",
+    title: "HorseGPT",
     statusBarStyle: "default",
   },
 };
