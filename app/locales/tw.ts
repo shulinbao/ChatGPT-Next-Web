6ef2212ce123224a7aac055835862b63e8b9aa1f
xlingoo <mahaonan1@hotmail.com>
2024-11-19 01:59:56 +0800

    Update tw.ts

--- app/locales/tw.ts
+++ app/locales/tw.ts
@@ -435,38 +435,33 @@ const tw = {
   },
   Maintitle: {
     Name: "HorseGPT Chat",
-  }, 
-  Subtitle: {<>
-            Chat website powered by <strong>HorseGPT</strong> project
-            <br />
-            <a
-              href="https://gpt.nu.ac.cn/"
-              target="_blank"
-              rel="noopener noreferrer"
-              style={{ color: 'inherit', textDecoration: 'underline' }}
-            >
-              Help
-            </a>{' '}
-            |{' '}
-            <a
-              href="https://oneapi.nu.ac.cn/"
-              target="_blank"
-              rel="noopener noreferrer"
-              style={{ color: 'inherit', textDecoration: 'underline' }}
-            >
-              HorseGPT API
-            </a>{' '}
-            |{' '}
-            <a
-              href="https://status.nu.ac.cn/"
-              target="_blank"
-              rel="noopener noreferrer"
-              style={{ color: 'inherit', textDecoration: 'underline' }}
-            >
-              Service Status
-            </a>
-          </>
   },
+  Subtitle: `Chat website powered by <strong>HorseGPT</strong> project
+    <br />
+    <a
+      href="https://gpt.nu.ac.cn/"
+      target="_blank"
+      rel="noopener noreferrer"
+      style="color: inherit; text-decoration: underline;"
+    >
+      Help
+    </a> | 
+    <a
+      href="https://oneapi.nu.ac.cn/"
+      target="_blank"
+      rel="noopener noreferrer"
+      style="color: inherit; text-decoration: underline;"
+    >
+      HorseGPT API
+    </a> | 
+    <a
+      href="https://status.nu.ac.cn/"
+      target="_blank"
+      rel="noopener noreferrer"
+      style="color: inherit; text-decoration: underline;"
+    >
+      Service Status
+    </a>`,
   Context: {
     Toast: (x: any) => `已設定 ${x} 條前置上下文`,
     Edit: "前置上下文和歷史記憶",
