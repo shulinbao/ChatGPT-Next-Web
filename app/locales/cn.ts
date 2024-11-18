74fb8199f7dbd892894d17b7db8ce5504d1d2546
xlingoo <mahaonan1@hotmail.com>
2024-11-19 01:02:31 +0800

    Update cn.ts

--- app/locales/cn.ts
+++ app/locales/cn.ts
@@ -9,10 +9,10 @@ const cn = {
   Error: {
     Unauthorized: isApp
       ? `😆 第一次来到 HorseGPT Chat?
-       \\ 1️⃣ 如果你不清楚什么是 HorseGPT Chat，[点击这里了解详情 🚀](${SAAS_CHAT_UTM_URL})`
+       \\ 1️⃣ 如果你想查阅使用方法，[请点击这里 🚀](${GUIDE_URL})`
       : `😆 第一次来到 HorseGPT Chat?
-       \ 1️⃣ 如果你不清楚什么是 HorseGPT Chat，[点击这里了解详情 🚀](${SAAS_CHAT_UTM_URL})
-       \ 2️⃣ 如果你已经购买了 HorseGPT API 的兑换码并生成了令牌，点击[这里](/#/auth)输入你的令牌就可以使用了 🔑
+       \ 1️⃣ 如果你想查阅使用方法，[请点击这里 🚀](${GUIDE_URLL})
+       \ 2️⃣ 如果你已经购买了 HorseGPT API 的兑换码并生成了令牌，点击[这里](/#/auth)输入你的令牌就可以使用啦 🔑
        `,
   },
   Auth: {
