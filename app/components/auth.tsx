6034cf9b9c782329d9e1034352c4d5ae3da01f56
xlingoo <mahaonan1@hotmail.com>
2024-11-30 16:51:19 +0800

    Update auth.tsx

--- app/components/auth.tsx
+++ app/components/auth.tsx
@@ -40,9 +40,9 @@ export function AuthPage() {
   }; // Reset access code to empty string
 
   useEffect(() => {
-    if (getClientConfig()?.isApp) {
-      navigate(Path.Settings);
-    }
+    // if (getClientConfig()?.isApp) {
+    //   navigate(Path.Settings);
+    // }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
